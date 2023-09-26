b7c6fef3821d3ad7815f842def8be90d66609705
Ohad <ohad.licht@gmail.com>
2023-09-27 00:43:48 +0300

    lambda function passes tests

---
+++ lambda/index.js
@@ -0,0 +1,23 @@
+const axios = require('axios')
+
+exports.handler = async (event) => {
+    try{
+        const axios = require('axios')
+        const query = event
+        if (query === "") {
+            return "Please provide a search query"
+        }
+        const url = `https://api.tvmaze.com/search/shows?q=${query}`
+        const res = await axios.get(url)
+        const shows = res.data.map((item) => {
+            return {
+                name: item.show.name,
+                summary: item.show.summary,
+                image: item.show.image.medium
+            }
+        })
+        return shows
+    } catch (err) {
+        return ("An error occurred")
+    }
+}
--- lambda/index.test.js
+++ lambda/index.test.js
@@ -1,11 +1,14 @@
-const handler = require('./index')
+const index = require('./index')
+const handler = index.handler
 
-test('Searching for a string will return results', async () => {
-    const res = await handler("star wars")
-    expect(res).toBeInstanceOf(Array)
-})
+describe('Testing the lambda function', () => {
+    it('Searching for a string will return results', async () => {
+        const res = await handler("star wars")
+        expect(res).toBeInstanceOf(Array)
+    })
 
-test('Searching for an empty string will return an error message', async () => {
-    const res = await handler("")
-    expect(res).toBe("Please provide a search query")
+    it('Searching for an empty string will return an error message', async () => {
+        const res = await handler("")
+        expect(res).toBe("Please provide a search query")
+    })
 })
